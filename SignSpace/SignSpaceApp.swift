91edce7ab5f6ec533a34463959f2e8f07718c4d4
Jaewon Park <jaewonparkk73@gmail.com>
2026-08-17 16:53:20 -0400

    Initial Commit

---
+++ SignSpace/SignSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignSpaceApp.swift
+//  SignSpace
+//
+//  Created by Jaewon Park on 8/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SignSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
